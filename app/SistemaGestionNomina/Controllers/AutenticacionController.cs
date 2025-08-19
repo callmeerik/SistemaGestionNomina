cc5f4904bb24af806f47a04881ab9152c8d7a332
Erik Andres <erikqtrs@gmail.com>
2025-08-18 21:58:10 -0500

    Agrego aplicacion sistema de gestion de nomina

---
+++ app/SistemaGestionNomina/Controllers/AutenticacionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaGestionNomina.Controllers
+{
+    public class AutenticacionController : Controller
+    {
+        // GET: Autenticacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
