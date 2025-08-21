490befafb2437d3d2b987086fbae12f3307e9b2a
farinango123-ctrl <farinango123-ctrl>
2025-08-21 17:45:32 -0500

    lsitar cargos,agregar,historial

--- app/SistemaGestionNomina/Models/Cargos.cs
+++ app/SistemaGestionNomina/Models/Cargos.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace SistemaGestionNomina.Models
+﻿namespace SistemaGestionNomina.Models
 {
     public class Cargos
     {
+        public int emp_no { get; set; }
+        public string ci { get; set; }
+        public string NombreCompleto { get; set; }
+        public string CargoActual { get; set; }
+        public string Desde { get; set; }
+        public string Hasta { get; set; }
+    }
+
+    public class Historial
+    {
+        public string Title { get; set; }
+        public string Desde { get; set; }
+        public string Hasta { get; set; }
+        public int DuracionMeses { get; set; }
     }
 }
